922f1c44810877b00e5fdc048207929512923916
沈俊攀 <754587525@qq.com>
2020-07-22 20:52:50 +0800

    3.0.0版本主菜单更新

---
+++ v3.0.0/source/main.py
@@ -0,0 +1,93 @@
+class mune:
+
+    def __init__(self):
+        while True:
+            self.__menu()
+            s = input('请输入你执行的操作的数字:')
+            try:  # 转换为整数
+                s = int(s)
+            except Exception:  # 为非数字的时候
+                print('请输入数字!' * 3)
+            if s > 6:  # 输入不存在的选项时
+                print('请输入存在的选项!' * 3)
+            elif s == 1:
+                print('爬虫已完成')
+            elif s == 2:
+                self.__black_user_operate()
+            elif s == 3:
+                print('设置cookie已保存')
+            elif s == 4:
+                self.__config_operate()
+            elif s == 5:
+                break
+
+    def __menu(self):
+        print('-' * 60)
+        print('pixiv自动下载程序')
+        print('-' * 60)
+        print('1.爬取开始')
+        print('2.黑名单操作')
+        print('3.cookie设置(当前__设置)')
+        print('4.配置修改')
+        print('5.退出')
+        print('-' * 60)
+
+    def __black_user_menu(self):
+        print('-' * 60)
+        print('黑名单操作')
+        print('-' * 60)
+        print('1.黑名单添加')
+        print('2.黑名单自运行(当前设置:)')
+        print('3.黑名单删除')
+        print('4.返回主菜单')
+        print('-' * 60)
+
+    def __black_user_operate(self):
+        self.__black_user_menu()
+        s = input('请输入你执行的操作的数字:')
+        try:  # 转换为整数
+            s = int(s)
+        except Exception:  # 为非数字的时候
+            print('请输入数字!' * 3)
+        if s > 4:  # 输入不存在的选项时
+            print('请输入存在的选项!' * 3)
+        elif s == 1:
+            print('黑名单已添加')
+        elif s == 2:
+            print("黑名单自运行设置已修改")
+        elif s == 3:
+            print("黑名单已删除")
+        elif s == 4:
+            pass
+
+    def __config_menu(self):
+        print('-' * 60)
+        print('配置修改操作')
+        print('-' * 60)
+        print('1.配置目录调整\n----修改后前面下载的数据如果迁移将无法使用classcifi下的lnk\n----如果要迁移数据请运行下面旧数据迁移选项')
+        print('2.socket配置(当前__配置)')
+        print('3.旧数据迁移\n----数据迁移会花费大量时间---慎重考虑!')
+        print('4.返回主菜单')
+        print('-' * 60)
+
+    def __config_operate(self):
+        self.__config_menu()
+        s = input('请输入你执行的操作的数字:')
+        try:  # 转换为整数
+            s = int(s)
+        except Exception:  # 为非数字的时候
+            print('请输入数字!' * 3)
+        if s > 4:  # 输入不存在的选项时
+            print('请输入存在的选项!' * 3)
+        elif s == 1:
+            print('配置目录已调整')
+        elif s == 2:
+            print("socket配置已生效")
+        elif s == 3:
+            print("数据已迁移完成")
+        elif s == 4:
+            pass
+
+
+if __name__ == '__main__':
+    mune()
